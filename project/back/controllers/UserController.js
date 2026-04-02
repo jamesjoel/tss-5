1bd05c4fe287ebe76c0c40bbfcb22e2df11828d1
jamesjoel <james.joelj@gmail.com>
2026-04-02 19:04:11 +0530

    custome validation

--- project/back/controllers/UserController.js
+++ project/back/controllers/UserController.js
@@ -5,9 +5,27 @@ let SaveUser = async(req, res)=>{
     delete req.body.repassword;
     
     req.body.password = sha1(req.body.password);
+    req.body.username = req.body.username.toLowerCase();
 
     let result = await User.create(req.body)
     res.send({success:true, result: result})
 }
 
-export {SaveUser}
+let GetAllUser = async(req, res)=>{
+    let result = await User.find();
+    res.send({success:true, result})
+}
+
+let UsernameExists = async(req, res)=>{
+    let u = req.params.u;
+    let result = await User.find({username : u }); // []
+    if(result.length==0){
+        res.send({success:true})
+    }else{
+        
+        res.send({success:false})
+    }
+}
+
+
+export {SaveUser, GetAllUser, UsernameExists}
